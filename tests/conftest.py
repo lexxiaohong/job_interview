dcc8e244aa8b1dd0c3552d6764bdd24875d1feff
lexxiaohong <lexxiaohong@gmail.com>
2025-06-26 11:38:56 +0700

    start test , can drop all table when test finished

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.database import Base
+
+TEST_DATABASE_URL = "sqlite+aiosqlite:///./test_unit.db"
+
+test_engine = create_async_engine(TEST_DATABASE_URL, echo=False, future=True)
+TestSessionLocal = sessionmaker(
+    bind=test_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+@pytest_asyncio.fixture(scope="function")
+async def db_session():
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    async with TestSessionLocal() as session:
+        yield session
+    async with test_engine.begin() as conn:
+        print()
+        print("Dropping all tables !!!!")
+        await conn.run_sync(Base.metadata.drop_all)
